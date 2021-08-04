a496b25c1c645e3413a58ea9b3c1b08c233d2904
Gia <giajordan@EMBI.ARIZONA.EDU>
2021-08-04 13:06:00 -0700

    Update Model building.py

--- Model building.py
+++ Model building.py
@@ -47,28 +47,59 @@ def buildHyperModel(hp):
     model=tf.keras.models.Model(inputs=resNet.input,outputs=output3)
      
    
-    model.compile(optimizer=optimize,loss=lossfxn,metrics=['mean_absolute_error'])
+    model.compile(optimizer=optimize,loss=lossfxn,metrics=['mean_absolute_error','mean_absolute_percentage_error'])
+    
+    return model
+
+def buildModel():
+    lR=0.95
+    optimize=tf.keras.optimizers.Adagrad(learning_rate=lR)
+    lossfxn='mse'
+    
+    
+    
+    #Model building: ResNet50V2 as base with dense layers before output
+    resNet= tf.keras.applications.ResNet50V2(include_top=False,weights='imagenet',input_shape=(224,224,3),pooling='avg')
+    flat=tf.keras.layers.Flatten()(resNet.output)
+    output1=tf.keras.layers.Dense(128,activation=acFunc,use_bias=True)(flat)
+    output2=tf.keras.layers.Dense(64,activation=acFunc,use_bias=True)(output1)
+    output3=tf.keras.layers.Dense(4,activation='linear',use_bias=True)(output2)
+    model=tf.keras.models.Model(inputs=resNet.input,outputs=output3)
+     
+    
+    
+        
+    
+        
+    #compile network and display summary
+    model.compile(optimizer=optimize,loss=lossfxn,metrics=['mean_absolute_error','mean_absolute_percentage_error']) 
+    model.summary()
+    
+        
+    
     
     return model
 
 
 
 
+hyperSearch=0
+
     
 #Training Parameters
-noEpochs=20#1030000
+noEpochs=1030000
 targetShape=(224,224)
 #lR=1.0#0.005
-tolerance=15
-batchSize=25
-delta=20
+tolerance=20
+batchSize=1
+delta=1
 
 #Location of repo, probably dont need later
 basePath=r"C:\Users\giajordan\Documents\GitHub\LC-PreProcessing"
 
 noUnits=128
 noLayers=1
-#acFunc='sigmoid'
+
 acFunc='sigmoid'
 #optimize=tf.keras.optimizers.Adam(learning_rate=lR)
 #optimize=tf.keras.optimizers.Adagrad(learning_rate=lR)
@@ -88,20 +119,20 @@ imPath=os.path.sep.join([basePath,r'Images'])
 annotPath=os.path.sep.join([basePath,r'Data.csv'])
 
 outputBase=r'Output'
-modelPath=os.path.sep.join([basePath,outputBase,'locatorOPT.h5'])
+modelPath=os.path.sep.join([basePath,outputBase,'locatorOpt.h5'])
 plotPath=os.path.sep.join([basePath,outputBase,'plot.png'])
 testNames=os.path.sep.join([basePath,outputBase,'testImages.txt'])
 
 
 # Training Params cont
 # # earlyCallback=tf.keras.callbacks.EarlyStopping(monitor='val_loss',min_delta=delta,patience=tolerance,restore_best_weights=True,mode='auto')
-earlyCallback=tf.keras.callbacks.EarlyStopping(monitor='mean_absolute_error', min_delta=delta, patience=tolerance,restore_best_weights=True,mode='auto')
+earlyCallback=tf.keras.callbacks.EarlyStopping(monitor='loss', min_delta=delta, patience=tolerance,restore_best_weights=True,mode='auto')
    
 # # checkpoint=tf.keras.callbacks.ModelCheckpoint(os.path.sep.join([basePath,outputBase,'Checkpoint.h5']),save_best_only=True,period=5)
 # # fitArgs={'callbacks':[earlyCallback,checkpoint],'verbose':1,'batch_size':batchSize,'epochs':noEpochs}
-fitArgs={'callbacks':[earlyCallback],'verbose':1,'batch_size':batchSize,'epochs':1030000}
+#fitArgs={'callbacks':[earlyCallback],'verbose':1,'batch_size':batchSize,'epochs':500000}
 
-#fitArgs={'verbose':1,'epochs':noEpochs,'batch_size':batchSize}
+fitArgs={'verbose':1,'epochs':500000,'batch_size':batchSize}
 
     
     
@@ -148,29 +179,19 @@ for row in rows:
 data=np.array(data,dtype='float32')/255.00
 targets=np.array(targets,dtype='float32')
 
-# #Model building: ResNet50V2 as base with dense layers before output
-# resNet= tf.keras.applications.ResNet152V2(include_top=False,weights='imagenet',input_shape=(224,224,3),pooling='avg')
-# flat=tf.keras.layers.Flatten()(resNet.output)
-# output1=tf.keras.layers.Dense(128,activation=acFunc,use_bias=True)(flat)
-# output2=tf.keras.layers.Dense(64,activation=acFunc,use_bias=True)(output1)
-# output3=tf.keras.layers.Dense(4,activation='linear',use_bias=True)(output2)
-# cnn=tf.keras.models.Model(inputs=resNet.input,outputs=output3)
+
  
-
-tuner=kt.Hyperband(buildHyperModel, \
-                      objective='mean_absolute_error', \
-                      max_epochs=noEpochs, \
-                      hyperband_iterations=1, \
-                      directory="HP Search", \
-                      seed=1738)
-
-    
+if hyperSearch:
+    tuner=kt.Hyperband(buildHyperModel, \
+                          objective='mean_absolute_error', \
+                          max_epochs=noEpochs, \
+                          hyperband_iterations=1, \
+                          directory="HP Search", \
+                          seed=1738)
 
     
-# #compile network and display summary
-# #cnn.compile(optimizer=optimize,loss=lossfxn,metrics=['accuracy']) 
-# cnn.compile(optimizer=optimize,loss=lossfxn,metrics=['mean_absolute_error']) 
-# cnn.summary()
+
+    
 
 
 skf=StratifiedKFold(n_splits=2,random_state=1738,shuffle=True)
@@ -182,27 +203,29 @@ for train_index, val_index in skf.split(targets, group):
     x_val=data[val_index,:,:,:,:]
     y_val=targets[val_index,:]
 
+if hyperSearch:
+    tuner.search(x_train,y_train, \
+            validation_data=[x_val,y_val], \
+            epochs=noEpochs, \
+            callbacks=[earlyCallback] \
+                 )
 
-tuner.search(x_train,y_train, \
-        validation_data=[x_val,y_val], \
-        epochs=noEpochs, \
-        callbacks=[earlyCallback] \
-             )
 
-
-cnn=tuner.get_best_models()[0]
-goodParams=tuner.get_best_hyperparameters()[0]
-    
-cnn.save(modelPath,save_format="h5")
+    cnn=tuner.get_best_models()[0]
+    goodParams=tuner.get_best_hyperparameters()[0]
+        
+    cnn.save(modelPath,save_format="h5")
 
 #separate labeled data into test and training inputs/outputs
 #x_train, x_test, y_train, y_test=train_test_split(data,targets,test_size=0.20,random_state=1738)
-skf=StratifiedKFold(n_splits=5,random_state=1738,shuffle=True)
+skf=StratifiedKFold(n_splits=3,random_state=1738,shuffle=True)
 hist=[]
 pxError=np.empty([skf.get_n_splits(),1])
 
-skf=StratifiedKFold(n_splits=3,random_state=1738,shuffle=True)
+#skf=StratifiedKFold(n_splits=3,random_state=1738,shuffle=True)
 for i, [train_index, test_index] in enumerate(skf.split(targets, group)):
+    
+    modelPath=os.path.sep.join([basePath,outputBase,'locatorStrat'+str(i)+'.h5'])
     print("TRAIN:", train_index, "TEST:", test_index)
     x_train=data[train_index,:,:,:,:]
     y_train=targets[train_index,:]
@@ -210,11 +233,13 @@ for i, [train_index, test_index] in enumerate(skf.split(targets, group)):
     x_test=data[test_index,:,:,:,:]
     y_test=targets[test_index,:]
 
-
+    
     #Train network
-    hist.append(cnn.fit(x=x_train,y=y_train,**fitArgs) )
+    cnn=buildModel()
+    hist.append(cnn.fit(x=x_train,y=y_train,**fitArgs))
+    cnn.save(modelPath,save_format="h5")
     
-    
+    file=open("Performance.txt",'a')
     
     #display loss and accuracy plots
     plt.plot(hist[i].history['loss'])
@@ -225,54 +250,60 @@ for i, [train_index, test_index] in enumerate(skf.split(targets, group)):
     plt.show()
     pxError[i]=hist[-1].history['mean_absolute_error'][-1]
     
-    # #Display testing labels, predictions, and differences
-    # print('\nTesting Data\n')
-    # pred=cnn.predict(x_test)
-    # print(y_test)
-    # print(pred)
-    # print(y_test-pred)
-    # cnn.evaluate(x_test,y_test)
+    #Display testing labels, predictions, and differences
+    print('\nTesting Data\n')
+    pred=cnn.predict(x_test)
+    print(y_test)
+    print(pred)
+    print(y_test-pred)
+    cnn.evaluate(x_test,y_test)
     
-    # #Display training labels, predictions, and differences
-    # print('\nTraining Data\n')
-    # pred=cnn.predict(x_train)
-    # print(pred)
-    # print(y_train)
-    # print(y_train-pred)
-
- 
-    # #iteratie through testing or training images and plot labels vs predictions
-    # for image, coords, targ in zip(x_test, pred, y_test):
-    #   #for image, coords, targ in zip(x_train, pred, y_train):
-    #       #print(coords)
-    #       sX,sY,eX,eY=coords;
-    #       sX2,sY2,eX2,eY2=targ;
+    file.write(str(pred))
+    file.write("\n")
+    file.write(str(y_test-pred))
+    file.write("\n\n")
+    file.close()
+    #Display training labels, predictions, and differences
+    print('\nTraining Data\n')
+    pred=cnn.predict(x_train)
+    print(y_train)
+    print(pred)
+    print(y_train-pred)
+    
+     
+    #iteratie through testing or training images and plot labels vs predictions
+    for image, coords, targ in zip(x_test, pred, y_test):
+      #for image, coords, targ in zip(x_train, pred, y_train):
+          #print(coords)
+          sX,sY,eX,eY=coords;
+          sX2,sY2,eX2,eY2=targ;
          
-    #       image=image[:,:,:,0]
+          image=image[:,:,:,0]
          
-    #       sX=sX*(225/5808)
-    #       eX=eX*(225/5808)
+          sX=sX*(225/5808)
+          eX=eX*(225/5808)
          
-    #       sY=sY*(225/4124)
-    #       eY=eY*(225/4124)
+          sY=sY*(225/4124)
+          eY=eY*(225/4124)
          
-    #       sX2=int(sX2*(225/5808))
-    #       eX2=int(eX2*(225/5808))
+          sX2=int(sX2*(225/5808))
+          eX2=int(eX2*(225/5808))
          
-    #       sY2=int(sY2*(225/4124))
-    #       eY2=int(eY2*(225/4124))
+          sY2=int(sY2*(225/4124))
+          eY2=int(eY2*(225/4124))
          
-    #       sX,sY,eX,eY=int(sX),int(sY),int(eX),int(eY)    
+          sX,sY,eX,eY=int(sX),int(sY),int(eX),int(eY)    
      
      
-    #       cv2.rectangle(image,(sX,sY),(eX,eY),(0,0,255),2)
-    #       cv2.rectangle(image,(sX2,sY2),(eX2,eY2),(255,0,0),2)
-    #       plt.figure()
-    #       plt.imshow(image)
- 
-    
+          cv2.rectangle(image,(sX,sY),(eX,eY),(0,0,255),2)
+          cv2.rectangle(image,(sX2,sY2),(eX2,eY2),(255,0,0),2)
+          plt.figure()
+          plt.imshow(image)
+     
         
-#print("Average Error: " + pxError.mean())
-
-
+        
+print("Average Error: " + str(pxError.mean()))
+file=open("Performance.txt",'a')
+file.write(str(pxError.mean()))
+file.close()
    
